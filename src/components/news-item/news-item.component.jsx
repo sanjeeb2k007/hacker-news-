9d7004382762448a1ddbc8df1434fcd12ed18563
sandora <sandora@publicisgroupe.net>
2020-06-17 18:07:50 +0530

    updated news-item and pagination component

--- src/components/news-item/news-item.component.jsx
+++ src/components/news-item/news-item.component.jsx
@@ -1,19 +1,63 @@
 import React from   'react';
 import './news-item.style.scss';
+ 
+class NewsItem extends React.Component {
 
-const NewsItem = ({title, url,author,created_at,points,num_comments}) => ( 
-    <div className={'news-item'}> 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hidden: false,
+            personvote: this.props.points
+        };
+    }
+
+    handleHideComponent = (props) => {
+        let newState = JSON.parse(JSON.stringify(this.state));
+        let sessionStorageItem =JSON.parse( window.sessionStorage.getItem("isHidden")) || {};
+        newState.hidden = true;
+        this.setState(newState);
+       sessionStorageItem[this.props['data-key']] = newState.hidden;
+        window.sessionStorage.setItem("isHidden", JSON.stringify(sessionStorageItem));       
+     }
+
+     componentDidMount () {
+       console.log(window.sessionStorage.getItem("isHidden"));
+     }
+
+     handleClick = props => async () => {
+        const data_vote = {
+          person_id: this.props['data-key'],
+          person_vote: this.state.personvote + 1
+        };
+    
+        const [{ vote }] = await this.getVotes(data_vote);
+    
+        this.setState({personvote:this.state.personvote + 1})
+      };
+    
+      // Mock call
+      getVotes = async data_vote => {
+        return await [{}];
+      };
+
+    render() {
+        return ( 
+            
+         <div className={`${this.state.hidden ? 'hidden' : ''} news-item`}> 
         <div className='content'>
-            {num_comments ? <span>{num_comments}</span> : ''}
-            {points ? <span>{points}</span> : ''}
-            {title ? <span className='title'>{title}</span>: ''}
-            <span className='url'>{url}</span>
-            <span className='author'>{author}</span>
-            <span className='date'>{new Date(`${created_at}`).toDateString()}</span>
-            <span className='hide'>[hide]</span>
+            {this.props.num_comments ? <span>{this.props.num_comments}</span> : ''}
+            <span className="upvote-btn" onClick={this.handleClick(this.props)}></span>
+            {this.props.points ? <span>{this.state.personvote}</span> : ''}
+            {this.props.title ? <span className='title'>{this.props.title}</span>: ''}
+            <span className='url'>{this.props.url}</span>
+            <span className='author'>{this.props.author}</span>
+            <span className='date'>{new Date(`${this.props.created_at}`).toDateString()}</span>
+            <span className='hide' onClick={this.handleHideComponent}>[hide]</span>
         </div>
     </div>
+    )}
 
-);
+}
 
 export default NewsItem;
--- src/components/news/news.component.jsx
+++ src/components/news/news.component.jsx
@@ -2,6 +2,7 @@ import React from   'react';
 import NewsItem from '../../components/news-item/news-item.component';
 import Pagination from '../../components/pagination/pagination.component';
 import './news.styles.scss';
+import {withRouter} from 'react-router-dom';
 
 class News extends React.Component {
     constructor() {
@@ -9,7 +10,7 @@ class News extends React.Component {
 
         this.state = {
             news: [],
-            pageNumber: 1
+            pageNumber: 1,
         };
     }
 
@@ -17,26 +18,36 @@ class News extends React.Component {
         const urlParam =  new URLSearchParams(window.location.search).get('p');
         fetch(`http://hn.algolia.com/api/v1/search?page=${urlParam ||this.state.pageNumber}`)
           .then(response => response.json())
-          .then(response => this.setState({news :response.hits, pageNumber:response.page }));
+          .then(response => { console.log(response.hits);this.setState({news :response.hits, pageNumber:response.page, hidden:true })});
       }
 
       nextClick  = () => {
-        fetch(`https://hn.algolia.com/api/v1/search?page=${this.state.pageNumber}`)
+        fetch(`https://hn.algolia.com/api/v1/search?page=${this.state.pageNumber + 1}`)
         .then(response => response.json())
-        .then(response => this.setState({news :response.hits, pageNumber:response.page + 1 }));
+        .then(response => {
+            this.setState(function (state, props) { 
+                return {  news :response.hits, pageNumber:response.page, hidden:true}
+            });
+        });
+        this.props.history.push(`${this.props.match.url}${'?p='}${this.state.pageNumber + 1}`);
       }
 
       prevClick = () => {
-        fetch(`https://hn.algolia.com/api/v1/search?page=${this.state.pageNumber}`)
+        fetch(`https://hn.algolia.com/api/v1/search?page=${this.state.pageNumber - 1}`)
         .then(response => response.json())
-        .then(response => this.setState({news :response.hits, pageNumber:response.page - 1 }));
+        .then(response => {
+            this.setState(function (state, props) { 
+                return {  news :response.hits, pageNumber:response.page, hidden:true}
+            });
+        });
+        this.props.history.push(`${this.props.match.url}${'?p='}${this.state.pageNumber - 1}`);
     }
 
     render() {
         return (
             <div className='news-container'>
                 {this.state.news.map(({objectID,...otherProps }) => (
-                        <NewsItem key={objectID} {...otherProps} />
+                    <NewsItem key={objectID} {...otherProps} data-key={objectID} />
                     ))}
                      <Pagination prevClick ={this.prevClick} nextClick = {this.nextClick} />
             </div>
@@ -46,4 +57,4 @@ class News extends React.Component {
     }
 }
 
-export default News;
+export default withRouter(News);
